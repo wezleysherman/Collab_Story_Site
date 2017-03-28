d3bce70a257bd49a7bc8f08279b940247806c87d
Wezley Sherman <wezleysherman@yahoo.com>
2017-03-27 21:39:03 -0700

    Update models.py

--- story/models.py
+++ story/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class StoryLine(models.Model):
 	line_text = models.CharField(max_length = 1024)
 	contributer = models.CharField(max_length = 128)
-	date_published = models.DateTimeField(default=datetime.now()) 
+	date_published = models.DateTimeField(default = datetime.now()) 
 
 	def __str__(self):
 		return self.line_text
@@ -16,7 +16,7 @@ class StoryLine(models.Model):
 class Story(models.Model):
 	story_title = models.CharField(max_length = 256)
 	sentences = models.ManyToManyField(StoryLine)
-	created = models.DateTimeField(default=datetime.now())
+	created = models.DateTimeField(default = datetime.now())
 
 	def __str__(self):
 		rtnStr = self.story_title + ": ".join(sen.line_text for sen in self.sentences.all()) 
